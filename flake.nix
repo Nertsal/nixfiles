a37417d86d6f1d5c591c92d4edf1084fb36277a5
Nertsal <sasha.kudasov04@gmail.com>
2023-05-24 16:06:37 +0300

    Fix home

--- flake.nix
+++ flake.nix
@@ -35,7 +35,7 @@
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
         # > Our main home-manager configuration file <
-        modules = [ ./home-manager/home.nix ];
+        modules = [ ./home/home.nix ];
       };
     };
 
